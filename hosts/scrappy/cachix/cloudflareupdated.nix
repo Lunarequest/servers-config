83160d72dbe6bf7d51a96103022dde6ea25ffedf
Luna D Dragon <advaith.madhukar@gmail.com>
2022-05-23 10:45:29 +0530

    nixfmt

--- hosts/scrappy/cachix/cloudflareupdated.nix
+++ hosts/scrappy/cachix/cloudflareupdated.nix
@@ -1,8 +1,6 @@
 {
   nix = {
-    binaryCaches = [
-      "https://cloudflareupdated.cachix.org"
-    ];
+    binaryCaches = [ "https://cloudflareupdated.cachix.org" ];
     binaryCachePublicKeys = [
       "cloudflareupdated.cachix.org-1:i0NdcmlQlaKpj5IXRrCLCjN/fMzMXe+WCq2lv+cY0Kk="
     ];
